ea566434e0c3181733199b7c37ee11619da0d17b
Denis Migranov <d.migranov@g.nsu.ru>
2019-10-29 21:51:29 +0700

    new files

---
+++ CyberUpBot/InfoModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord.Commands;
+
+namespace CyberUpBot
+{
+
+    // Keep in mind your module **must** be public and inherit ModuleBase.
+    // If it isn't, it will not be discovered by AddModulesAsync!
+    public class InfoModule : ModuleBase<SocketCommandContext>
+    {
+
+    }
+}
