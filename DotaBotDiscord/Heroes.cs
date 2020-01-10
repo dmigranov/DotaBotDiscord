d9982de875e2de3961c83e7bf7d7b3f7ca82333d
Denis Migranov <d.migranov@g.nsu.ru>
2020-01-10 16:03:07 +0700

    heroes/hero

---
+++ DotaBotDiscord/Heroes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotaBotDiscord
+{
+    class Heroes
+    {
+        public List<Hero> heroes { get; set; }
+    }
+
+    class Hero
+    {
+        public string name { get; set; }
+        public int id { get; set; }
+        public string localized_name { get; set; }
+    }
+}
