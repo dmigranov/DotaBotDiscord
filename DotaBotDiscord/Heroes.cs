10b52b8e2fd3b507c51e6d3fad77750c921ba81e
Denis Migranov <d.migranov@g.nsu.ru>
2020-01-10 16:30:06 +0700

    мапа

--- DotaBotDiscord/Heroes.cs
+++ DotaBotDiscord/Heroes.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DotaBotDiscord
 {
     class Heroes
     {
-        public List<Hero> heroes { get; set; }
+        private List<Hero> _heroes;
+        private Dictionary<int, Hero> heroesMap;
+        public List<Hero> heroes { 
+            get { return _heroes; }
+            set { 
+                _heroes = value;
+                heroesMap = _heroes.ToDictionary(x => x.id, x => x);
+            }
+        }
+
+        public Hero GetHero(int id)
+        {
+            return heroesMap[id];
+        }
     }
 
     class Hero
