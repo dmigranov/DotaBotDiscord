d66a43fe59dbccf843c7723e85dc001558e9abc1
Denis Migranov <d.migranov@g.nsu.ru>
2020-01-12 14:58:47 +0700

    очистка public

--- DotaBotDiscord/PublicModule.cs
+++ DotaBotDiscord/PublicModule.cs
@@ -40,24 +40,6 @@ namespace DotaBotDiscord
             await ReplyAsync($"{user.Mention}: {user.ToString()}");
         }
 
-        // Ban a user
-        /*
-        
-        [Command("ban")]
-        [RequireContext(ContextType.Guild)]
-        // make sure the user invoking the command can ban
-        [RequireUserPermission(GuildPermission.BanMembers)]
-        // make sure the bot itself can ban
-        [RequireBotPermission(GuildPermission.BanMembers)]
-        public async Task BanUserAsync(IGuildUser user, [Remainder] string reason = null)
-        {
-            await user.Guild.AddBanAsync(user, reason: reason);
-            await ReplyAsync("ok!");
-        }
-
-        */
-
-
         // [Remainder] takes the rest of the command's arguments as one argument, rather than splitting every space
         [Command("echo")]
         [Summary("Повторяет сообщение, отправленное аргументом")]
