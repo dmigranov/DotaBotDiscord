63b593ed074edaaaa34ff226f9efdf7e530500c4
Denis Migranov <d.migranov@g.nsu.ru>
2020-01-12 11:12:51 +0700

    удалил лишнеее из интерактив

--- DotaBotDiscord/InteractiveModule.cs
+++ DotaBotDiscord/InteractiveModule.cs
@@ -10,24 +10,7 @@ namespace DotaBotDiscord
 {
     public class InteractiveModule : InteractiveBase
     {
-
         public static OpenDotaApi _openDota { get; set; }
-
-
-        // NextMessageAsync will wait for the next message to come in over the gateway, given certain criteria
-        // By default, this will be limited to messages from the source user in the source channel
-        // This method will block the gateway, so it should be ran in async mode.
-        [Command("next", RunMode = RunMode.Async)]
-        public async Task Test_NextMessageAsync()
-        {
-            await ReplyAsync("What is 2+2?");
-            var response = await NextMessageAsync();
-            if (response != null)
-                await ReplyAsync($"You replied: {response.Content}");
-            else
-                await ReplyAsync("Прошло слишком много времени. Начните регистрацию заново.");
-        }
-
 
         [Command("register", RunMode = RunMode.Async)]
         [Summary("Заполнение анкеты (пишите в личные сообщения боту)")]
